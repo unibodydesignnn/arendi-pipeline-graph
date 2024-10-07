315b24d932ff8c68ad35a9fc87bc4e4f0d133424
unibodydesignnn <unibodydesignnn@gmail.com>
2024-10-07 05:43:34 +0100

    initial commit

---
+++ Arendi Pipeline Graph/Arendi Pipeline Graph/Arendi_Pipeline_GraphApp.swift
@@ -0,0 +1,17 @@
+//
+//  Arendi_Pipeline_GraphApp.swift
+//  Arendi Pipeline Graph
+//
+//  Created by Melih Mete on 07/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct Arendi_Pipeline_GraphApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Arendi Pipeline Graph/Arendi Pipeline Graph/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Arendi Pipeline Graph
+//
+//  Created by Melih Mete on 07/10/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
